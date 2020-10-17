df0dc96489522e3cf18fee0699f41a9e4769b8d6
Sonny <Sonnyhaack@gmail.com>
2020-10-17 21:29:03 +0200

    Login

---
+++ Assignment1/Data/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using Models;
+
+namespace Data
+{
+    public interface IUserService
+    {
+        User ValidateUser(string userName, string password);
+    }
+}
--- Assignment1/Startup.cs
+++ Assignment1/Startup.cs
@@ -2,13 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Assignment1.Authentication;
+using Data;
+using Data.Implementation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Security.Claims;
 
 namespace Assignment1
 {
@@ -25,6 +30,30 @@ namespace Assignment1
         public void ConfigureServices(IServiceCollection services) {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            
+            // Login
+            services.AddScoped<IUserService, InMemoryUserService>();
+            services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+            
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("MustBeVIA", a =>
+                    a.RequireAuthenticatedUser().RequireClaim("Domain", "via.dk"));
+
+                options.AddPolicy("SecurityLevel4", a =>
+                    a.RequireAuthenticatedUser().RequireClaim("Level", "4", "5"));
+
+                options.AddPolicy("MustBeTeacher", a =>
+                    a.RequireAuthenticatedUser().RequireClaim("Role", "Teacher"));
+
+                options.AddPolicy("SecurityLevel2", policy =>
+                    policy.RequireAuthenticatedUser().RequireAssertion(context =>
+                    {
+                        Claim levelClaim = context.User.FindFirst(claim => claim.Type.Equals("Level"));
+                        if (levelClaim == null) return false;
+                        return int.Parse(levelClaim.Value) >= 2;
+                    }));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
